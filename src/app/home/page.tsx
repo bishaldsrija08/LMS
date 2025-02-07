26aa29b6202b80f63b6eac1057555cdf297ee6c0
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-02-07 22:20:24 +0545

    Google Login Implementation,Session

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,31 +1,30 @@
-"use client"
-import { signIn } from "next-auth/react"
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from 'next/image'
 
-function Hello(){
+function Hello() {
+  const { data: session } = useSession();
+  if (session) {
     return (
-<div className="bg-gradient-to-r from-blue-500 to-green-500 flex flex-col h-screen">
-  <nav className="bg-white py-6 px-8 flex justify-between items-center">
-    <a href="#" className="text-xl font-bold text-gray-800">Brand Name</a>
-    <ul className="flex space-x-6">
-      <li><a href="#" className="text-gray-800">Home</a></li>
-      <li><a href="#" className="text-gray-800">About</a></li>
-      <li><a href="#" className="text-gray-800">Services</a></li>
-      <li><a href="#" className="text-gray-800">Contact</a></li>
-    </ul>
-  </nav>
-  <header className="flex-grow flex flex-col justify-center items-center px-8">
-    <h1 className="text-5xl font-bold text-white text-center mb-8">Welcome to Brand Name</h1>
-    <p className="text-white text-lg text-center mb-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-      sagittis lobortis sapien, eu tincidunt metus semper eget. Sed at malesuada arcu. Pellentesque habitant morbi
-      tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-    <button onClick={()=>signIn("google")} className="text-lg bg-yellow-500 hover:bg-yellow-600 text-white py-4 px-6 rounded-lg font-bold shadow-md =">Get
-      started
-    </button>
-  </header>
-</div>
-
-    )
+      <>
+        <Image src={session.user?.image} alt="lamo" width={100} height={100}/>
+        <h1>Welcome, {session.user?.name}</h1>
+        <h1>Your email is, {session.user?.email}</h1>
+        <button  onClick={() => signOut()}>Sign Out</button>
+      </>
+    );
+  }
+  return (
+    <>
+      <button
+        onClick={() => signIn("google")}
+        className="text-lg bg-yellow-500 hover:bg-yellow-600 text-white py-4 px-6 rounded-lg font-bold shadow-md ="
+      >
+        Get
+        started
+      </button>
+    </>
+  );
 }
 
-
-export default Hello
+export default Hello;
