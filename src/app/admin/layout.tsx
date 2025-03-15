e5427445afb7a9a9c4ea1b302458617f79cadc08
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-03-15 15:45:23 +0545

    Authorization Client

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,13 +1,20 @@
-import Dashboard from "@/components/dashboard/Dashboard"
+"use client";
+import Dashboard from "@/components/dashboard/Dashboard";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
-
-function AdminLayout({children}:Readonly<{children : React.ReactNode}>){
-    return(
-        <Dashboard>
-            {children}
-            
-        </Dashboard>
-    )
+function AdminLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  const { data: session, status } = useSession();
+  useEffect(() => {
+    if (status === "loading") return;
+//@ts-ignore
+    if (!session || session.user.role !== "admin") {
+      redirect("/");
+    }
+  }, [session, status]);
+  if (status === "loading" || status === "unauthenticated") return <h1>Loading...!</h1>
+  return <Dashboard>{children}</Dashboard>;
 }
 
-export default AdminLayout
+export default AdminLayout;
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,30 @@
 import dbConnect from "@/database/connection";
 import User from "@/database/models/user.schema";
-import { Session } from "inspector/promises";
-import NextAuth, { AuthOptions } from "next-auth";
+import NextAuth, { Session } from "next-auth";
+import { } from "next-auth";
 import GoogleProvider from "next-auth/providers/google"
 
-export const authOptions: AuthOptions = {
-    providers: [
+interface IToken{name : string,email:string,picture:string,sub:string,id:string,role:string}
+//@ts-ignore
+export const authOptions:AuthOptions = {
+    providers : [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        })
+            clientId : process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret : process.env.GOOGLE_CLIENT_SECRET as string
+        }), 
     ], 
-    
-    secret: process.env.NEXTAUTH_SECRET,
-    callbacks: {
-      async  signIn({user}) : Promise<boolean> {
+    secret : process.env.NEXTAUTH_SECRET,
+    callbacks : {
+       async signIn({user}:{user : {name : string, email : string, image : string}}) : Promise<boolean>{
             try {
                 await dbConnect()
-            const existingUser =  await User.findOne({email: user.email}) //findone returns object
-            if(!existingUser){
+               const existingUser = await User.findOne({email : user.email}) // return object {username : "sdfdf",email:"sdf"} , {}\
+            
+               if(!existingUser){
                 await User.create({
-                    username: user.name,
-                    email:user.email,
-                    profileImage:user.image
+                    username : user.name, 
+                    email : user.email, 
+                    profileImage : user.image,
                 })
             }
             return true
@@ -30,14 +32,30 @@ export const authOptions: AuthOptions = {
                 console.log(error)
                 return false
             }
-        },
-       async session(session:Session, user:any){
-          const  data = await User.findById(user.id)
-            session.user.role = data.role  || "student"
-            return session
+        }, 
+      async jwt({token}:{token : IToken}){
+        await dbConnect()
+        const user = await User.findOne({
+            email : token.email
+        })
+        console.log(user,"USER")
+        if(user){
+            token.id = user._id
+            token.role = user.role
+        }
+        return token 
+      },
+       async session({session,token}:{session:Session,token:IToken}){
+           if(token){
+            session.user.id = token.id; 
+            session.user.role = token.role 
+           }
+          
+           return session;
+        
         }
     }
 }
+//@ts-ignore
 const handler = NextAuth(authOptions)
-
 export {handler as GET, handler as POST}
